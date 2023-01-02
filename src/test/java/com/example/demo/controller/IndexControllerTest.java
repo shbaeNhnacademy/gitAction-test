ab2388e5635b43958c25f7ab20484ceb9e2c671b
배수한 <qotngks152@naver.com>
2023-01-02 19:34:39 +0900

    Add IndexControllerTest

---
+++ src/test/java/com/example/demo/controller/IndexControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IndexControllerTest {
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void home() {
+    }
+}
